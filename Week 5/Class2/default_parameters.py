34895e34ba636a30ef6d9a62e358abe642aeeb31
shayroy <2shayroy@gmail.com>
2018-11-29 12:00:12 -0500

    Week 5 class 2

--- Week 5/Class2/default_parameters.py
+++ Week 5/Class2/default_parameters.py
@@ -8,11 +8,11 @@ hello("Brendan")
 
 hello()
 
-def favorites (col="white", food="lasagna")
+def favorites (col="white", food="lasagna"):
 # this allows entering one value, but will go to the first one, which would be mistake if a food entered.
 # so need to force them
 
-fav(food="chocolate")
-fav(col="Blue, food="x")
+favorites(food="chocolate")
+favorites(col="Blue, food="x")
 
 
