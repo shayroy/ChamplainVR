0d49f237b176ac720ef699c1255abe3afeec6bf2
shayroy <2shayroy@gmail.com>
2018-12-05 17:18:59 -0500

    Week 6 class 1 homework

--- Week 6/Class1/homework8-7.py
+++ Week 6/Class1/homework8-7.py
@@ -8,3 +8,22 @@ Add an optional parameter to make_album() that allows you to store the
 number of tracks on an album. If the calling line includes a value for the number
 of tracks, add that value to the album’s dictionary. Make at least one new
 function call that includes the number of tracks on an album."""
+
+def make_album(artist_name, album_title, number_of_tracks=0):
+    """Return a dictionary of information about albums."""
+    entry = {'artist':artist_name, 'title':album_title, 'tracks':number_of_tracks}
+    return entry
+
+entry0 = make_album('The Beatles', 'Abby Road')
+entry1 = make_album('The Rolling Stones', 'Exile on Main St.')
+entry2 = make_album('Joni Mitchell', 'Blue')
+entry3 = make_album('Nirvana', 'Nevermind', '8')
+
+print(entry0, entry1, entry2, entry3)
+
+
+
+
+
+
+
