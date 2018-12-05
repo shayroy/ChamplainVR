a744dfbedb2c7a1331e4a34c634c8827b40b4432
shayroy <2shayroy@gmail.com>
2018-12-05 18:03:09 -0500

    Week 6 class 1 homework

--- Week 6/Class1/homework8-8.py
+++ Week 6/Class1/homework8-8.py
@@ -3,16 +3,34 @@ loop that allows users to enter an album’s artist and title. Once you have tha
 information, call make_album() with the user’s input and print the dictionary
 that’s created. Be sure to include a quit value in the while loop."""
 
-def make_album(artist_name, album_title, number_of_tracks=0):
+def make_album(artist_name, album_title):
     """Return a dictionary of information about albums."""
     entry = {'artist':artist_name, 'title':album_title}
-    if number_of_tracks:
-        entry['number_of_tracks'] = number_of_tracks
     return entry
 
-entry0 = make_album('The Beatles', 'Abby Road')
-entry1 = make_album('The Rolling Stones', 'Exile on Main St.')
-entry2 = make_album('Joni Mitchell', 'Blue')
-entry3 = make_album('Nirvana', 'Nevermind', '8')
+while True:
 
-print(entry0, entry1, entry2, entry3)
+    print("\nPlease tell me the name of an artist and the title of one of their albums. ")
+    print("(enter 'q' at any time to quit)")
+
+    artist = input("Artist name: ")
+    if artist =='q':
+        break
+
+    album = input("Album title: ")
+    if album == 'q':
+        break
+
+    print(make_album(artist, album))
+
+
+
+
+
+
+# entry0 = make_album('The Beatles', 'Abby Road')
+# entry1 = make_album('The Rolling Stones', 'Exile on Main St.')
+# entry2 = make_album('Joni Mitchell', 'Blue')
+# entry3 = make_album('Nirvana', 'Nevermind', '8')
+
+# print(entry0)
