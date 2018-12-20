9f0555da65aba92c3a3989d124118fa7e7ab31a8
shayroy <2shayroy@gmail.com>
2018-12-20 12:52:34 -0500

    Week 8 Class2

--- Week 8/Class2/Item.py
+++ Week 8/Class2/Item.py
@@ -1,24 +1,17 @@
 class Item:
 
-   # last_serial_used = 0
-
-    def __init__(self, sku, name, price, taxable):
+      def __init__(self, sku, name, price, taxable):
         """Instantiating the items"""
 
         self.__sku = sku
         self.__name = name
         self.__price = price
         self.__taxable = taxable
-       # self.__item_number = Item.last_serial_used + 1
-       # Item.last_serial_used = self.item_number
 
-   # @staticmethod
-   # def print_last_serial_used():
-   #     print (Item.last_serial_used)
 
     def print_item(self):
         """Printing the description of the item"""
-        self.item_description=("{0} {1} ${2} {3} {4}".format(self.__name,
+        self.item_description=("{} {} ${:0,.2f} {} {}".format(self.__name,
                                             ("."*(20-len(self.__name))),
                                             self.__price,
                                             (" "*(7-len(str(self.__price)))),
@@ -30,35 +23,40 @@ class Item:
         return self.__price
 
 
-    def item_tax_amount(self):
+    def item_gst_amount(self):
         """Returns tax amount"""
         if self.__taxable:
-            self.__tax_amount = self.__price * .1495
+            self.__gst_amount = self.__price * .05
         else:
-            self.__tax_amount = 0
-        return self.__tax_amount
+            self.__gst_amount = 0
+        return self.__gst_amount
+
+    def item_pst_amount(self):
+        """Returns tax amount"""
+        if self.__taxable:
+            self.__pst_amount = self.__price * .09975
+        else:
+            self.__pst_amount = 0
+        return self.__pst_amount
 
     def get_item_total(self):
-        self.__item_total = self.__price + self.__tax_amount
+        self.__item_total = self.__price + self.__gst_amount + self.__pst_amount
         return self.__item_total
 
+"""Test area:
 
-
-a1 = Item(12345678, "first item", 2.00, True)
-# instantiating the first item
-a2 = Item(23456789, "second item", 3.00, False)
-# instantiating the second item
+a1 = Item(12345678, "first item", 2.00, True)# instantiating the first item
+a2 = Item(23456789, "second item", 3.00, False)# instantiating the second item
 
 print(a1.print_item())
 print(a2.print_item())
 print(a1.item_base_price())
-print(a1.item_tax_amount())
+print(a1.item_gst_amount())
+print(a1.item_pst_amount())
 print(a2.item_base_price())
-print(a2.item_tax_amount())
+print(a2.item_gst_amount())
+print(a2.item_pst_amount())
 print(a1.get_item_total())
-print(a2.get_item_total())
+print(a2.get_item_total())"""
 
-
-
-# still need to print sku, format price and determine Quebec tax formula.
 
