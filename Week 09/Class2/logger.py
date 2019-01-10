8b92c533fc45771c62cc9d82b9e2fd1c12fb40bd
shayroy <2shayroy@gmail.com>
2019-01-10 12:56:06 -0500

    Week 9 Class 2 and Order assignement.

---
+++ Week 09/Class2/logger.py
@@ -0,0 +1,19 @@
+#The timelog file will be called by the functions.
+filename = 'writelog.txt'
+#datetime will be used in the open and close timestamps by the logger.
+#from datetime import *
+im
+
+def writelog(filename):
+    """"This function logs time in timelog."""
+    opentime = datetime.datetime.now()
+    with open(filename, 'a') as f:
+    current_date_time = datetime.datetime.now
+    f.write(current_date_time)
+    sleep (5)
+    f.write (current_date_time)
+        with close(filename, 'a') as f:
+        f.write(datetime.now)
+
+
+datetime.
