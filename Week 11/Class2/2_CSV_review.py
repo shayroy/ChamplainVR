4cab40adc7ba10e778db48b6dca4a65f4ad38f4f
shayroy <2shayroy@gmail.com>
2019-01-30 10:43:52 -0500

    Finalize Week 11 Class2 assignment Jan 30

--- Week 11/Class2/2_CSV_review.py
+++ Week 11/Class2/2_CSV_review.py
@@ -9,9 +9,10 @@ with open ("biostats.csv", "rt") as csv_file:
     for row in reader:
         if row_counter != 1:
             row[3] = round(float(row[3])*2.54)
+            #print (row[3])
             row[4] = round(float(row[4])*.454)
             csv_data.append(row)
-            row_counter += 1
+        row_counter += 1
 
 with open('biostats_metric.csv', 'w', newline='') as csv_metric_file:
     writer = csv.writer(csv_metric_file)
